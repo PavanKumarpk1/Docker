d3b93750336b0d398dc150ea8515a8fe7e6f1461
Pavan <pavankumarppk24@gmail.com>
2026-05-08 21:02:06 +0530

    initial deploy

---
+++ api_1/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/write', methods=['POST'])
+def write_file():
+    # Get the text sent from the frontend text box
+    data = request.json.get('text_entry')
+    
+    if data:
+        with open("/data/memory.txt", "a") as f:
+            f.write(data + "\n")
+        return jsonify({"message": "Saved successfully!"}), 201
+    return jsonify({"error": "No text provided"}), 400
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
